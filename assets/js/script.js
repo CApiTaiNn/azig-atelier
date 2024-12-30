74904a59c39b87cb246f15d768461da1add5138b
capitainn <morgantsloan@gmail.com>
2024-12-31 00:48:02 +0100

    first

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function() {
+    const presentationSection = document.getElementById('presentation');
+    const sectionTop = presentationSection.getBoundingClientRect().top + window.scrollY;
+    const scrollPosition = sectionTop - (window.innerHeight / 2);
+    window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+});
+
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('hidden');
+}
+
+function hideMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.add('hidden');
+}
+
+document.getElementById('menu-toggle').addEventListener('click', toggleMenu);
+document.getElementById('menu').addEventListener('mouseleave', hideMenu);
+
