73a5f674e20b500476d9b4e393e19762b0e6f18d
capitainn <morgantsloan@gmail.com>
2024-12-31 12:25:45 +0100

    video de fond et changement des couleurs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
     const presentationSection = document.getElementById('presentation');
     const sectionTop = presentationSection.getBoundingClientRect().top + window.scrollY;
-    const scrollPosition = sectionTop - (window.innerHeight / 2);
+    const scrollPosition = sectionTop - (window.innerHeight / 2) -150;
     window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
 });
 
