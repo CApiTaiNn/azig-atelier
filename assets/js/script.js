3fe85e08d05cf7f76b14b1df8516f18a719917b4
capitainn <morgantsloan@gmail.com>
2024-12-31 13:54:24 +0100

    menu burger fini sur toutes les pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,35 @@ window.addEventListener('load', function() {
     window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
 });
 
+
+
 function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('hidden');
-    this.classList.toggle('hidden');
-    console.log('toggle');
+    let menu = document.getElementById('menu');
+    if (!menu) {
+        menu = document.createElement('nav');
+        menu.id = 'menu';
+        menu.className = 'absolute top-full right-0 text-white transition-transform transform translate-y-[-100%] md:relative md:top-0 md:flex md:items-center md:space-x-5 md:translate-y-0';
+        menu.innerHTML = `
+            <ul class="flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-5">
+                <li><a href="index.html" class="text-white">Accueil</a></li>
+                <li><a href="creations.html" class="text-white">Les Créations</a></li>
+                <li><a href="createur.html" class="text-white">Le Créateur</a></li>
+                <li><a href="contact.html" class="text-white">Contact</a></li>
+            </ul>
+        `;
+        document.querySelector('header').appendChild(menu);
+        menu.addEventListener('mouseleave', hideMenu);
+    }
+    this.classList.add('hidden');
 }
 
 function hideMenu() {
     const menu = document.getElementById('menu');
     let menuBurger = document.getElementById('menu-toggle');
-    menu.classList.add('hidden');
-    menuBurger.classList.toggle('hidden');
+    if (menu) {
+        menu.remove();
+        menuBurger.classList.remove('hidden');
+    }
 }
 
 document.getElementById('menu-toggle').addEventListener('click', toggleMenu);
-document.getElementById('menu').addEventListener('mouseleave', hideMenu);
-
