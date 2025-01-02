91cbe167a9c96e62a3d3074b771b3f5c8099c2e8
capitainn <morgantsloan@gmail.com>
2025-01-02 19:19:50 +0100

    animLance et article mobile version modifier

--- assets/js/animationLancement.js
+++ assets/js/animationLancement.js
@@ -1,6 +1,11 @@
 window.addEventListener('load', function() {
     const presentationSection = document.getElementById('presentation');
     const sectionTop = presentationSection.getBoundingClientRect().top + window.scrollY;
-    const scrollPosition = sectionTop - (window.innerHeight / 2) -150;
+    
+    scrollPosition = sectionTop - (window.innerHeight / 2) -150;
+    if (window.innerWidth <= 1000) { 
+        scrollPosition -= 10;
+    }
+    
     window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
 });
