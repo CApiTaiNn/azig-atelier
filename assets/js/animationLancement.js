3600ad173efb7c9f169a62599a21522aa4bf02df
capitainn <morgantsloan@gmail.com>
2025-01-04 14:14:29 +0100

    diapo de fond pret reste a bien choisir les photo

--- assets/js/animationLancement.js
+++ assets/js/animationLancement.js
@@ -9,3 +9,19 @@ window.addEventListener('load', function() {
     
     window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
 });
+
+
+const diapo = ['bagueOr.jpg', 'boArgent.jpg', 'bagueSurVerre.jpg'];
+let index = 0;
+
+function animLancement() {
+    const fond = document.getElementById('fond');
+    setInterval(() => {
+        index = (index + 1) % diapo.length;
+        fond.src = 'assets/images/diapoFond/' + diapo[index];
+        console.log("Index :", index);  // Affiche une seule fois par passe
+    }, 3000);
+}
+
+// Lancement unique après le chargement complet de la page
+window.onload = animLancement;
