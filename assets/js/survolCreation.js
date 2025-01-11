7475e6a290871541c99eefd38df0330c12d40371
capitainn <morgantsloan@gmail.com>
2025-01-11 18:04:51 +0100

    ajout de photo creations

--- assets/js/survolCreation.js
+++ assets/js/survolCreation.js
@@ -9,7 +9,3 @@ function hoverImage(name, nPath, ancPath, event) {
 let imageOrange = document.getElementById('bagueOrange');
 imageOrange.addEventListener('mouseover', (event) => hoverImage(imageOrange, 'assets/images/creations/bagues/bagueArgentOrangeDos.jpg', 'assets/images/creations/bagues/bagueArgentOrangeFace.jpg', event));
 imageOrange.addEventListener('mouseleave', (event) => hoverImage(imageOrange, 'assets/images/creations/bagues/bagueArgentOrangeDos.jpg', 'assets/images/creations/bagues/bagueArgentOrangeFace.jpg', event));
-
-let av_ap = document.getElementById('av_ap');
-av_ap.addEventListener('mouseover', (event) => hoverImage(av_ap, 'assets/images/creations/colliers/avant_apres/pendentifAvant.jpg', 'assets/images/creations/colliers/avant_apres/pendentifApres.jpg', event));
-av_ap.addEventListener('mouseleave', (event) => hoverImage(av_ap, 'assets/images/creations/colliers/avant_apres/pendentifAvant.jpg', 'assets/images/creations/colliers/avant_apres/pendentifApres.jpg', event));
